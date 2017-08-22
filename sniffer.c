dfd825d74dff233e0d1f90ff3b0f77969fadb228
mike <mike@lptp.ubuntu>
2017-08-22 16:56:47 -0500

    funcionando falta procesar trama

--- sniffer.c
+++ sniffer.c
@@ -10,35 +10,68 @@
 #include <sys/types.h>
 #include <net/if.h>
 
+
+#define BUFFER_SIZE 2000
+
 int main(int argc, char const *argv[])
 {
-	char buff[2000];
+	if(argc < 2){
+		perror("Falta argumento de numero de paquetes a capturar")
+		exit(-1);
+	}
 
+	char buff[BUFFER_SIZE];
+
+	int socket_raw;
+	int flag;
+	int nPaquetes=atoi(argv[0]);
+
+	flag=create_socket(socket_raw);
+	if(flag == 0)	//si flag es 0 entoces el socket se creo bien
+	{
+		int len=recvfrom(sock,&buff,sizeof(buff),0,&saddr,&addr_size);
+		procesar_trama(&buff,BUFFER_SIZE)
+	}
+		
+	return 0;
+}
+
+int procesar_trama(char *buffer, int buffer_size){
+	
+}
+
+int create_socket(int *socket)
+{
 	struct sockaddr_in source,dest;
 	struct sockaddr saddr;
 	struct ifreq ethreq;
 
-	int sock= socket(PF_PACKET,SOCK_RAW,ETH_P_ALL);
-	if(sock)
-		printf("socket bien sock: %d\n",sock);
+	int sock= socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
+	if(sock < 0){
+		perror("Error al abrir el socket %d",sock)
+		return -1;
+	}
 
 	strncpy(ethreq.ifr_name,"eno1",IFNAMSIZ);
 
-	printf("ethreq.ifr_name: %s\n",ethreq.ifr_name );
-
 	int io=ioctl(sock,SIOCGIFFLAGS,&ethreq);
-	printf("iocl: %d\n",io);
+	if(io<0)
+	{
+		perror("Error al obtener banderas %d",io)
+		return -1;
+	}
 
 	ethreq.ifr_flags |=IFF_PROMISC;
 
 
 	io=ioctl(sock,SIOCSIFFLAGS,&ethreq);
-	printf("ioScl: %d\n",io);
+	if(io<0)
+	{
+		perror("Error al asignar banderas %d",io)
+		return -1;
+	}
 
-	int addr_size=sizeof(saddr);
-printf("addrsize %d  buffersize: %d\n",addr_size,sizeof(buff));
-	int len=recvfrom(sock,&buff,sizeof(buff),0,&saddr,&addr_size);
-	printf("%d\n",len );
-	printf("%s\n",buff );
-	return 0;
+	*socket=sock;
+	return 0
+	
 }
