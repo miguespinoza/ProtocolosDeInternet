d02d218bcc6ba24643dc926e7c0ac0d7c61439ee
miguel espinoza <miguespinoza@outlook.com>
2017-08-22 00:46:33 -0500

    bugfix

--- sniffer.c
+++ sniffer.c
@@ -19,16 +19,26 @@ int main(int argc, char const *argv[])
 	struct ifreq ethreq;
 
 	int sock= socket(PF_PACKET,SOCK_RAW,ETH_P_ALL);
+	if(sock)
+		printf("socket bien sock: %d\n",sock);
 
 	strncpy(ethreq.ifr_name,"eno1",IFNAMSIZ);
 
-	ioctl(sock,SIOCGIFFLAGS,&ethreq);
+	printf("ethreq.ifr_name: %s\n",ethreq.ifr_name );
+
+	int io=ioctl(sock,SIOCGIFFLAGS,&ethreq);
+	printf("iocl: %d\n",io);
 
 	ethreq.ifr_flags |=IFF_PROMISC;
 
-	ioctl(sock,SIOCSIFFLAGS,&ethreq);
+
+	io=ioctl(sock,SIOCSIFFLAGS,&ethreq);
+	printf("ioScl: %d\n",io);
+
 	int addr_size=sizeof(saddr);
-	recvfrom(sock,buff,sizeof(buff),0,&saddr,&addr_size);
+printf("addrsize %d  buffersize: %d\n",addr_size,sizeof(buff));
+	int len=recvfrom(sock,&buff,sizeof(buff),0,&saddr,&addr_size);
+	printf("%d\n",len );
 	printf("%s\n",buff );
 	return 0;
 }
