4f41f5704e73f8fd0c8175df58658118887892d8
mike <mike@lptp.ubuntu>
2017-08-22 20:47:50 -0500

    sniffer funcionando, detecta protocolo de paquete

--- sniffer.c
+++ sniffer.c
@@ -10,68 +10,129 @@
 #include <sys/types.h>
 #include <net/if.h>
 
+#include<netinet/ip_icmp.h>   //Provides declarations for icmp header
+#include<netinet/udp.h>   //Provides declarations for udp header
+#include<netinet/tcp.h>   //Provides declarations for tcp header
+#include<netinet/ip.h>    //Provides declarations for ip header
+
 
 #define BUFFER_SIZE 2000
 
+int create_socket();
+int procesar_trama(unsigned char *buffer, int buffer_size);
+
+int socket_raw;
+
+
 int main(int argc, char const *argv[])
 {
+	struct sockaddr saddr;
 	if(argc < 2){
-		perror("Falta argumento de numero de paquetes a capturar")
+		perror("Falta argumento de numero de paquetes a capturar");
 		exit(-1);
 	}
-
-	char buff[BUFFER_SIZE];
-
-	int socket_raw;
+	char *buff=(unsigned char *) malloc(sizeof(unsigned char)*BUFFER_SIZE);
 	int flag;
-	int nPaquetes=atoi(argv[0]);
-
-	flag=create_socket(socket_raw);
+	int nPaquetes=atoi(argv[1]);
+	flag=create_socket();
 	if(flag == 0)	//si flag es 0 entoces el socket se creo bien
 	{
-		int len=recvfrom(sock,&buff,sizeof(buff),0,&saddr,&addr_size);
-		procesar_trama(&buff,BUFFER_SIZE)
-	}
+		int addr_size =sizeof(saddr);
+		for(int index=0;index<nPaquetes;index++){
+			int len=recvfrom(socket_raw,buff,BUFFER_SIZE,0,&saddr,&addr_size);
+			if(len<0){
+				perror("error al recibir paquete");
+				exit(-1);
+			}	
+			printf("l: %d\n",len);
+
+			procesar_trama(buff,BUFFER_SIZE);
+		}
 		
+
+	}
+	else{
+		perror("error al abrir el socket");
+	}
 	return 0;
 }
 
-int procesar_trama(char *buffer, int buffer_size){
+int procesar_trama(unsigned char *buffer, int buffer_size)
+{
+	/*MAC fuente 
+	Dirección MAC destino 
+	Longitud de la trama 
+	Longitud de carga útil (datos y relleno) 
+	Determinar si la dirección de destino es una dirección de
+		 unidifusión, difusión o multidifusió*/
+
 	
+	uint16_t type = 0;
+
+	memcpy(&type,&buffer[12],sizeof(uint16_t));
+	type = (type>>8) | (type<<8);
+
+
+	if(type <= 0x05dc){
+		printf("ETHERNET II -- ");
+	}
+	else{
+		switch(type)
+		{
+			case 0x0800:
+				printf("IPv4 ");
+				break;
+			case 0x86dd:
+				printf("IPv6 -- ");	
+				break;
+			case 0x0806:
+				printf("ARP -- ");
+				break;
+			case 0x8808:
+				printf("Control de flujo ethernet -- ");
+				break;
+			case 0x88e5: 
+				printf("Seguridad MAC -- ");
+				break;
+			default:
+			printf("Otro -- ");
+
+		}
+	}
+
 }
 
-int create_socket(int *socket)
+int create_socket()
 {
 	struct sockaddr_in source,dest;
-	struct sockaddr saddr;
+	
 	struct ifreq ethreq;
 
-	int sock= socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
-	if(sock < 0){
-		perror("Error al abrir el socket %d",sock)
+	socket_raw= socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
+	if(socket_raw < 0){
+		perror("Error al abrir el socket ");
 		return -1;
 	}
 
 	strncpy(ethreq.ifr_name,"eno1",IFNAMSIZ);
 
-	int io=ioctl(sock,SIOCGIFFLAGS,&ethreq);
+	int io=ioctl(socket_raw,SIOCGIFFLAGS,&ethreq);
 	if(io<0)
 	{
-		perror("Error al obtener banderas %d",io)
+		perror("Error al obtener banderas ");
 		return -1;
 	}
 
 	ethreq.ifr_flags |=IFF_PROMISC;
 
 
-	io=ioctl(sock,SIOCSIFFLAGS,&ethreq);
+	io=ioctl(socket_raw,SIOCSIFFLAGS,&ethreq);
 	if(io<0)
 	{
-		perror("Error al asignar banderas %d",io)
+		perror("Error al asignar banderas ");
 		return -1;
 	}
 
-	*socket=sock;
-	return 0
+	return 0;
 	
 }
